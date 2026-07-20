41789bef91567eec9de79d67df6fd5dc58a8b9d1
FidanUmudova <fidanumudova05@gmail.com>
2026-07-20 13:09:53 +0400

    feat: complete folder structure and base files

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF6C5CE7);
+  static const Color background = Color(0xFFF8F9FA);
+  static const Color textDark = Color(0xFF2D3436);
+  static const Color textGrey = Color(0xFFA4B0BE);
+  static const Color cardBackground = Colors.white;
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'core/constants/app_colors.dart';
+import 'features/onboarding/screens/onboarding_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Profile App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.background,
+        useMaterial3: true,
+      ),
+      home: const OnboardingScreen(),
+    );
+  }
+}
